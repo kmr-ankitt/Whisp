ab3caf865dd8b3d0bd5f83ead111befaf7a154b7
kmr-ankitt <sahuankit.murhu@gmail.com>
2024-06-20 08:46:34 +0530

    Connecting database

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isConnected?: number;
+};
+
+const connection: connectionObject = {};
+
+async function dbConnect(): Promise<void> {
+    if(connection.isConnected){
+        console.log("Already connected to database");
+        return ;
+    }
+    try {
+        const db = await mongoose.connect(
+            process.env.MONGODB_URI || "" , {}
+        )
+
+        connection.isConnected = db.connections[0].readyState;
+
+        console.log("db connected successfully.");
+        
+    } catch (error) {
+        console.log("Database connection failed.");
+        
+        process.exit(1)
+    }
+}
+
+export default dbConnect;
