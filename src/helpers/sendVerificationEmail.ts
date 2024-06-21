21b99a4241132114e1c6b6711447ae9c7c91c8f7
kmr-ankitt <sahuankit.murhu@gmail.com>
2024-06-21 09:51:55 +0530

    Next Auth

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -2,7 +2,7 @@ import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
 import { ApiResponse } from "@/types/ApiResponse";
 
-export async function sendVerificatiomEmail(
+export async function sendVerificationEmail(
     email:string,
     username : string,
     verfiyCode : string,
